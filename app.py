5ce9bb0e1346429374168243ceed8ecf3f3a2810
colin <colinnoahpeter@gmail.com>
2022-10-28 16:06:36 +0800

    ignore

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import time
+from unittest.mock import DEFAULT
+import requests
 
 import redis
-from flask import Flask
+from flask import Flask, request
+from constants import *
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -21,5 +24,16 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
+    x = requests.get(f'http://metaphorpsum.com/paragraphs/{DEFAULT_PARAGRAPHS}/{DEFAULT_SENTENCES}')
+
+    try:
+        app.logger.info(x.text)
+    except:
+        app.logger('Something went wrong')
+    app.logger.info(DEFAULT_SENTENCES)
+    app.logger.info(DEFAULT_PARAGRAPHS)
+    app.logger.info(x.status_code)
+    app.logger.info('request logged')
+    app.logger.info(request.args)
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
